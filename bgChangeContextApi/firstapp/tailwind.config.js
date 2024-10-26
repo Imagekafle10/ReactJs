fc581d1ae7bb8c847e4a8bb105024168de3d3193
Imagekafle10 <hpimage10@gmail.com>
2024-10-26 09:04:17 +0545

    toogltbtn
    10/26/2024

---
+++ bgChangeContextApi/firstapp/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This should point to your React components
+  ],
+  darkMode : "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
